ac083015b42fa4add2deb5e1e32f5c0f7154653d
RubiyaHud <rubiya.hud@gmail.com>
2025-06-01 13:01:08 +0600

    complete career obj

--- src/components/RotatingCircle.jsx
+++ src/components/RotatingCircle.jsx
@@ -1,20 +1,23 @@
-import React from "react";
+
 import { motion } from "framer-motion";
-
-import { FaHtml5, FaCss3Alt, FaJs, FaReact } from "react-icons/fa";
-import { SiRedux, SiNextdotjs } from "react-icons/si";
-
+import { FcReading } from "react-icons/fc";
+import { FcComboChart } from "react-icons/fc";
+import { FaRegHandshake } from "react-icons/fa";
+import { MdOutlineSettingsSuggest } from "react-icons/md";
+import { HiOutlineLightBulb } from "react-icons/hi";
+import { GiStumpRegrowth } from "react-icons/gi";
+import { GiBrainstorm } from "react-icons/gi";
 
 const RotatingCircle = () => {
   // const items = ["HTML", "CSS", "JS", "React", "Redux", "Next.js"]; // Text for each circle
   const items = [
-    { label: "HTML", icon: <FaHtml5 size={24} color="#E34F26" /> },
-    { label: "CSS", icon: <FaCss3Alt size={24} color="#1572B6" /> },
-    { label: "JS", icon: <FaJs size={24} color="#F7DF1E" /> },
-    { label: "React", icon: <FaReact size={24} color="#61DAFB" /> },
-    { label: "Redux", icon: <SiRedux size={24} color="#764ABC" /> },
-    { label: "Next.js", icon: <SiNextdotjs size={24} color="#000000" /> },
-    { label: "Next.js", icon: <SiNextdotjs size={24} color="#000000" /> },
+    { icon: <FcReading size={100} color="#E34F26"/> },
+    { icon: <FcComboChart size={100} color="#1572B6" /> },
+    { icon: <FaRegHandshake size={100} color="#F7DF1E" /> },
+    { icon: <MdOutlineSettingsSuggest size={100} color="#61DAFB" /> },
+    { icon: <HiOutlineLightBulb size={100} color="#764ABC" /> },
+    { icon: <GiStumpRegrowth size={100} color="#97D552" /> },
+    { icon: <GiBrainstorm size={100} color="#FF8CB7" /> },
   ];
 
   const radius = 150;
@@ -31,7 +34,7 @@ const RotatingCircle = () => {
         }}
       >
         {/* Center dot */}
-        <div className="w-6 h-6 bg-yellow-400 rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10" />
+        {/* <div className="w-6 h-6 bg-yellow-400 rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10" /> */}
 
         {items.map(({ label, icon }, i) => {
           const angle = (360 / items.length) * i;
@@ -41,14 +44,13 @@ const RotatingCircle = () => {
           return (
             <div
               key={i}
-              className="w-30 h-30 bg-blue-500 text-white flex flex-col items-center justify-center rounded-full absolute text-2xl font-normal shadow-md"
+              className="w-30 h-30 border border-amber-300 text-white flex flex-col items-center justify-center rounded-full absolute text-[16px] font-normal shadow-md"
               style={{
                 top: `calc(50% + ${y}px - 40px)`, // 40 = half of 80 (w-20,h-20)
                 left: `calc(50% + ${x}px - 40px)`,
               }}
             >
               {icon}
-              <span className="mt-1">{label}</span>
             </div>
           );
         })}
--- src/components/layouts/CareerObjLayout.jsx
+++ src/components/layouts/CareerObjLayout.jsx
@@ -14,11 +14,13 @@ const CareerObjLayout = ({ id }) => {
                     <div className='w-[400px]'>
                         <RotatingCircle />
                     </div>
-                    <div className='w-[600px]'>
+                    <div className='w-[650px]'>
                         <h3 className='text-[#EAB308] text-[42px] font-bold leading-16 pb-7'> Career Objective </h3>
                         <p className='text-amber-50 text-[16px] font-normal leading-7'>
-                            Passionate and detail-focused frontend developer with a PhD in Computer Science, eager to turn complex ideas into intuitive, high-performance web interfaces. Skilled in modern frameworks like React, Redux, and Next.js, with a strong eye for design using Tailwind CSS and SASS. I thrive on building clean, scalable code and creating seamless user experiences. Now seeking an opportunity to apply my academic background and hands-on project experience to real-world challenges, collaborate with a forward-thinking team, and continue growing through practical development work
+                            Passionate and detail-focused frontend developer with a PhD in Computer Science, eager to turn complex ideas into intuitive, high-performance web interfaces. Skilled in modern frameworks like React, Redux, and Next.js, with a strong eye for design using Tailwind CSS and SASS. I thrive on building clean, scalable code and creating seamless user experiences through <span className="text-[#EAB308]"> Technical Excellence </span> and <span className="text-[#EAB308]"> Frontend Innovation </span>. Committed to <span className="text-[#EAB308]"> Continuous Learning</span> and <span className="text-[#EAB308]"> Skill Growth </span>, I value <span className="text-[#EAB308]">Team Collaboration</span> and believe in the power of shared knowledge to drive success. Now seeking an opportunity to apply my academic background and hands-on project experience to real-world challenges, contribute to a <span className="text-[#EAB308]"> Forward-Thinking </span> team, and pursue ongoing <span className="text-[#EAB308]"> Professional Development </span>.
                         </p>
+
+
                     </div>
 
 
